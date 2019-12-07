d29d3066a9be5dd68516bfbc30063ef902a8eff5
Omar chadidi <omarchadidi@gmail.com>
2019-12-07 15:01:38 +0100

    added update endpoint.

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -3,6 +3,7 @@ const Posts = require('../db/posts')
 
 const router = express.Router()
 
+// Get all
 router.get('/api/v1/posts', (req, res, next) => {
   Posts.find({}, (err, posts) => {
     if (err) throw err
@@ -14,6 +15,21 @@ router.get('/api/v1/posts', (req, res, next) => {
   })
 })
 
+// Get by ID
+router.get('/api/v1/posts/:id', (req, res, next) => {
+  const id = req.params.id
+  Posts.findById(id, (err, post) => {
+    if (err) throw err
+
+    return res.status(200).send({
+      success: 'true',
+      message: 'post retrieved successfully',
+      post
+    })
+  })
+})
+
+// add new post
 router.post('/api/v1/posts', (req, res) => {
   if (!req.body.title) {
     return res.status(400).send({
@@ -40,17 +56,52 @@ router.post('/api/v1/posts', (req, res) => {
   })
 })
 
-router.get('/api/v1/posts/:id', (req, res, next) => {
+// Update posts by ID
+router.put('/api/v1/posts/:id', (req, res, next) => {
   const id = req.params.id
-  Posts.findById(id, (err, post) => {
-    if (err) throw err
 
-    return res.status(200).send({
-      success: 'true',
-      message: 'post retrieved successfully',
-      post
+  // get a user with ID of 1
+  Posts.findById(id, (err, post) => {
+    if (err) throw err;
+
+    const update = {
+      title: req.body.title,
+      body: req.body.body
+    }
+    if (update.title)
+      post.title = req.body.title
+    if (update.body)
+      post.body.body = req.body.body
+
+    post.save((err) => {
+      if (err) throw err
+       console.log('Post successfully updated!');
     })
-  })
+  });
 })
 
 module.exports = router
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
