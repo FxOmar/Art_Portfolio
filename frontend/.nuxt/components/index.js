6ee84531f4224a3575373b3ff6194c4952077d59
Omar <omar@fevrok.com>
2020-09-07 18:14:19 +0100

    update .gitignore  👌✅

--- frontend/.nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Navbar } from '../..\\components\\ui\\Navbar.vue'
-
-export const LazyNavbar = import('../..\\components\\ui\\Navbar.vue' /* webpackChunkName: "components_ui/Navbar" */).then(c => c.default || c)
